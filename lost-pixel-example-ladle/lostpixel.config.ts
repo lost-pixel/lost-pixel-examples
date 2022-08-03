ebead139890fe5f55d83b9ca4b8d43395c8561e1
d-ivashchuk <dimaiv@me.com>
2022-08-03 21:08:03 +0200

    add ladle exmple

---
+++ lost-pixel-example-ladle/lostpixel.config.ts
@@ -0,0 +1,8 @@
+import { CustomProjectConfig } from "lost-pixel";
+
+export const config: CustomProjectConfig = {
+  ladleShots: {
+    ladleUrl: "http://172.17.0.1:61000",
+  },
+  generateOnly: true,
+};
