408f7e5eb11325209c360702d5f47cf87c252fda
David Madner <david.madner@gmx.net>
2022-09-17 21:33:21 +0200

    Update example-ladle/lostpixel.config.ts

--- example-ladle/lostpixel.config.ts
+++ example-ladle/lostpixel.config.ts
@@ -2,7 +2,7 @@ import { CustomProjectConfig } from "lost-pixel";
 
 export const config: CustomProjectConfig = {
   ladleShots: {
-    ladleUrl: "http://127.0.0.1:61000",
+    ladleUrl: "http://172.17.0.1:61000",
   },
   generateOnly: true,
 };
